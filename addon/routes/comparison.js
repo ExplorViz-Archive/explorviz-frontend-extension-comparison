bbce81bfbe4eef3bfabceb31d8f590b7dafb8c76
Josefine Wegert <jweg@informatik.uni-kiel.de>
2018-02-15 16:28:09 +0100

    test component

--- addon/routes/comparison.js
+++ addon/routes/comparison.js
@@ -2,9 +2,9 @@ import Route from '@ember/routing/route';
 import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
 
 export default Route.extend( AuthenticatedRouteMixin, {
-  actions: {
+  //actions: {
   // @Override BaseRoute -> if you change between components in your extension,
   // resetRoute (example in frontend: change between landscape and application perspective); BaseRoute.extend()
 
-}
+//}
 });
