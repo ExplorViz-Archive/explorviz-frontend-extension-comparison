14016c436c0d0aed29d88ead7cae225982446af7
Josefine Wegert <jweg@informatik.uni-kiel.de>
2018-02-16 11:56:35 +0100

    refactoring, delete comparison-test

--- addon/components/comparison-test.js
+++
@@ -1,7 +0,0 @@
-import Component from '@ember/component';
-import layout from '../templates/components/comparison-test';
-
-export default Component.extend({
-  layout,
-  message:'comparison test message'
-});
