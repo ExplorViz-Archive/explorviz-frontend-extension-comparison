b68429b1783a1afa551ab042a59af0fbc4e1ec7e
Josefine Wegert <jweg@informatik.uni-kiel.de>
2018-02-22 15:16:40 +0100

    reset namespace when leaving extension routes

--- addon/routes/comparison.js
+++ addon/routes/comparison.js
@@ -6,8 +6,12 @@ export default VisualizationRoute.extend( AuthenticatedRouteMixin, {
   model(){
     this.get("store").adapterFor('landscape').set('namespace', 'extension/comparison');
     return this.get("store").queryRecord("landscape", "landscape/merged");
-  }
+  },
 
+//resets the namespace, if the user leaves the comparison route
+deactivate(){
+  this.get("store").adapterFor('landscape').set('namespace', 'landscape');
+}
 //   actions: {
 //   // @Override BaseRoute -> if you change between components in your extension,
 //   // resetRoute (example in frontend: change between landscape and application perspective); BaseRoute.extend()
