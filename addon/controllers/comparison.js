a689f17554e80318e33013c069872b25b2592b30
Josefine Wegert <jweg@informatik.uni-kiel.de>
2018-04-09 14:40:10 +0200

    refactoring: stop rewriting components/.. from frontend core

--- addon/controllers/comparison.js
+++ addon/controllers/comparison.js
@@ -1,5 +1,15 @@
+import Ember from 'ember';
 import visualizationController from 'explorviz-frontend/controllers/visualization';
 
 export default visualizationController.extend({
 
+    mergedRepo: Ember.inject.service('merged-repository'),
+
+  showVersionbar() {
+    this.set('renderingService.showVersionbar', true);
+  },
+
+  showMergedLandscape: Ember.computed('mergedRepo.mergedApplication', function() {
+    return !this.get('mergedRepo.mergedApplication');
+  }),
 });
--- addon/routes/comparison.js
+++ addon/routes/comparison.js
@@ -1,23 +1,25 @@
-import VisualizationRoute from 'explorviz-frontend/routes/visualization';
+import Ember from 'ember';
+import BaseRoute from 'explorviz-frontend/routes/base-route';
 import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
 
-export default VisualizationRoute.extend( AuthenticatedRouteMixin, {
+const {inject} = Ember;
 
-  model(){
-    this.get("store").adapterFor('landscape').set('namespace', 'extension/comparison');
-    return this.get("store").queryRecord("landscape", "landscape/merged");
+export default BaseRoute.extend( AuthenticatedRouteMixin, {
+
+  mergedRepo: inject.service('merged-repository'),
+  renderingService: inject.service('renderingService'),
+  viewImporter: inject.service('view-importer'),
+
+actions: {
+  resetRoute(){
+    this.set('viewImporter.importedURL', false);
+    this.get('renderingService').reSetupScene();
+    this.controller.set('mergedRepo.mergedApplication', null);
   },
 
-//resets the namespace, if the user leaves the comparison route
-deactivate(){
-  this.get("store").adapterFor('landscape').set('namespace', 'landscape');
+  didTransition() {
+    this.set('renderingService.showTimeline', false);
+    this.set('renderingService.showVersionbar', true);
+  }
 }
-//   actions: {
-//   // @Override BaseRoute -> if you change between components in your extension,
-//   // resetRoute (example in frontend: change between landscape and application perspective); BaseRoute.extend()
-// resetRoute(){
-//   visualizationController.send('resetView');
-//   visualizationController.set('landscapeRepo.latestLandscape', null);
-// }
-// }
 });
