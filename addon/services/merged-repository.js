9d07521b8f26cf3e106b8895c9b56d704aaa7612
Josefine Wegert <jweg@informatik.uni-kiel.de>
2018-04-09 14:17:11 +0200

    repository for merged application and landscape added

---
+++ addon/services/merged-repository.js
@@ -0,0 +1,9 @@
+import LandscapeRepo from 'explorviz-frontend/services/repos/landscape-repository';
+
+export default LandscapeRepo.extend({
+
+  mergedLandscape: null,
+
+  mergedApplication: null,
+
+});
